38e8f5f391b8ef9c4182ec93ae7e6f93678f59db
Andrew R Chen <hello@ndrewr.com>
2020-01-29 16:34:26 -0800

    Add base code for new action "prod-check-deploy".

---
+++ prod-check-deploy/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -l
+
+# Create the encrypted Prod auth url file
+echo $1 > prod_auth_url.txt.enc
+
+# Get the private url from environment variable, create required file for cmd
+echo "Setting up Prod Connection..."
+openssl enc -d -aes-256-cbc -md md5 -in prod_auth_url.txt.enc -out prod_auth_url.txt -k $2
+
+# Authenticate to salesforce Prod org
+echo "Authenticating..."
+sfdx force:auth:sfdxurl:store -f prod_auth_url.txt -a Prod && rm prod_auth_url.txt
+
+#Convert to MDAPI format for validation against prod
+echo "Converting to MDAPI format..."
+sfdx force:source:convert -d validate_prod -r force-app
+#Simulate deployment to prod & run all tests
+echo "Validating against production by simulating a deployment & running all tests..."
+sfdx force:mdapi:deploy -c -d validate_prod -u Prod -l RunLocalTests -w -1
+
+echo "CI Validate Prod action completed."
