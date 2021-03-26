9fed335ec5a563fefe4cc536da4a457325dae8de
Scott Covert <scott.b.covert@gmail.com>
2021-03-25 23:27:57 -0400

    Specify args embedded within cmd var based on action in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,16 @@ enc_auth_file=${ENC_AUTH_FILE:-sfdx_auth_url.txt.enc}
 run_tests=${RUN_TESTS:-NotSet}
 scratch_def_file=${SCRATCH_DEF_FILE:-config/project-scratch-def.json}
 test_level=${TEST_LEVEL:-NoTestRun}
-cmd="${action} -e ${enc_auth_file} -r ${run_tests} -s ${scratch_def_file} -t ${test_level}"
+
+if [ "${action}" = "deploy" ]
+then
+  cmd="${action} -e ${enc_auth_file} -r ${run_tests} -t ${test_level}"
+elif [ "${action}" = "test" ]
+then
+  cmd="${action} -e ${enc_auth_file} -s ${scratch_def_file}"
+elif [ "${action}" = "validate" ]
+then
+  cmd="${action} -e ${enc_auth_file}"
+fi
 
 eval $cmd
