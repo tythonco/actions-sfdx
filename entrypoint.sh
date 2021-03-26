ad4e3c3ffdd8be7515a090d473bb8e622f41e3c5
Scott Covert <scott.b.covert@gmail.com>
2021-03-26 18:21:58 -0400

    Renamed action param to command

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,30 +2,30 @@
 
 set -eu
 
-action=${ACTION:-validate}
+command=${COMMAND:-validate}
 auth_file_key=${AUTH_FILE_KEY:-NotSet}
 enc_auth_file=${ENC_AUTH_FILE:-sfdx_auth_url.txt.enc}
 run_tests=${RUN_TESTS:-NotSet}
 scratch_def_file=${SCRATCH_DEF_FILE:-config/project-scratch-def.json}
 test_level=${TEST_LEVEL:-NoTestRun}
 
-if { [ "${action}" = "deploy" ] || [ "${action}" = "test-scratch" ] || [ "${action}" = "validate" ]; } && [ "${auth_file_key}" = "NotSet" ];
+if { [ "${command}" = "deploy" ] || [ "${command}" = "test-scratch" ] || [ "${command}" = "validate" ]; } && [ "${auth_file_key}" = "NotSet" ];
 then
 	echo "ERROR: Please specify auth file decryption key with auth_file_key argument"
 	exit 1
 fi
 
-if [ "${action}" = "deploy" ]
+if [ "${command}" = "deploy" ]
 then
-  cmd="${action} -a ${auth_file_key} -e ${enc_auth_file} -r ${run_tests} -t ${test_level}"
-elif [ "${action}" = "test-scratch" ]
+  cmd="${command} -a ${auth_file_key} -e ${enc_auth_file} -r ${run_tests} -t ${test_level}"
+elif [ "${command}" = "test-scratch" ]
 then
-  cmd="${action} -a ${auth_file_key} -e ${enc_auth_file} -s ${scratch_def_file}"
-elif [ "${action}" = "validate" ]
+  cmd="${command} -a ${auth_file_key} -e ${enc_auth_file} -s ${scratch_def_file}"
+elif [ "${command}" = "validate" ]
 then
-  cmd="${action} -a ${auth_file_key} -e ${enc_auth_file}"
+  cmd="${command} -a ${auth_file_key} -e ${enc_auth_file}"
 else
-  cmd="${action}"
+  cmd="${command}"
 fi
 
-eval ${cmd}
+eval "${cmd}"
